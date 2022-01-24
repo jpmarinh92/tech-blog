e78d52e02ab8827340af52588ee3bac085875649
JPMarinH92 <jpmarin92@hotmail.com>
2022-01-24 11:07:12 -0500

    connect to jawsdb

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# tech-blog
+<h1 align="center"> Tech Blog </h1>
+
+<p align="center"> Welcome to this tech blog! </p>
+
+## Links
+
+- [Repo](https://github.com/jpmarinh92/tech-blog "Tech Blog")
+
+- [Video Demostration](https://drive.google.com/file/d/1uG8C3EKfE2tEgr6h7IcBQPXp9Ws-xRtO/view "Tech Blog")
+
+## Built With
+
+- MySql
+- Sequelize
+- Node
+- Express
+- Handlebars
+
+## Author
+
+**Juan Pablo Marin Higuita**
+
+- [Profile](https://github.com/jpmarinh92 "Juan Pablo Marin Higuita")
--- server.js
+++ server.js
@@ -6,7 +6,20 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
+// const sequelize = require("./config/connection");
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
+
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
